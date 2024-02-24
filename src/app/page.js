d80b7eac215e3519ba53872761da2abccfdbb8be
Hector <adeniyi.olaitanhector@yahoo.com>
2024-02-24 23:25:20 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,33 @@
 import Image from "next/image";
+import blocks from "../../public/blocks2.png";
 import styles from "./page.module.css";
+import AOSInit from "./AOSInit";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+      <AOSInit/>
+      <Image className={styles.logo} width={250} height={50} src="/HypertroveVenturesLogo.png" alt="Hypertrove Ventures" data-aos="fade-in" />
+      <div className={styles.flex}>
+        <div className={styles.flexLeft}>
+          <h1 className={styles.h1} data-aos="zoom-up">Elevate Your<br/>Cardano Project</h1>
+          <p className={styles.p} data-aos="fade-up">
+            At HyperTrove Ventures, we're dedicated to propelling Cardano-based projects and their surrounding ecosystems into new heights. By investing in the brightest teams within the crypto space, we aim to fast-track your project's growth and success.
           </p>
-        </a>
+          <button className={styles.button} data-aos="zoom-in" data-aos-delay="500" data-aos-offset="0">Pitch To Us</button>
+        </div>
+        <div className={styles.flexRight}>
+          <Image
+            src={blocks}
+            className={styles.image}
+            alt="cubes blocks"
+            layout="responsive"
+            data-aos="fade-left"
+          />
+        </div>
+        
       </div>
+
     </main>
   );
 }
